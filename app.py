537f24c454e92b8963856de2af29cbc5c3315819
wesley lima <153243264+limawx@users.noreply.github.com>
2024-07-16 01:05:48 -0300

    app.py

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 import pyautogui
 from time import sleep
 
-#1 - clicar em digitar meu usuário (x=686, y=382)
-pyautogui.click(x=794, y=383,duration=1)
+# 1 - clicar em digitar meu usuário (x=686, y=382)
+pyautogui.click(x=794, y=383, duration=1)
 pyautogui.write('wesley')
-#2 - clicar em digitar minha senha (x=723, y=411)
-pyautogui.click(x=723, y=411,duration=1)
+
+# 2 - clicar em digitar minha senha (x=723, y=411)
+pyautogui.click(x=723, y=411, duration=1)
 pyautogui.write('1234')
-#3 - clicar em "entrar" (x=594, y=431)
-pyautogui.click(x=594, y=431,duration=1)
-#4 - extrair cada produto
-with open('produtos.txt','r') as arquivo:
+
+# 3 - clicar em "entrar" (x=594, y=431)
+pyautogui.click(x=594, y=431, duration=1)
+
+# 4 - extrair cada produto
+with open('produtos.txt', 'r') as arquivo:
     for linha in arquivo:
-        produto = linha.split(',')[0]
-        quantidade = linha.split(',')[1]
-        preco = linha.split(',')[2]
-    #  1 - clicar e digitar produto (x=470, y=373)
-    pyautogui.click(x=470, y=373,duration=0.5)
-    pyautogui.write(produto)
-    #  2 - clicar e digitar quantidade (x=442, y=404)
-    pyautogui.click(x=442, y=404,duration=0.5)
-    pyautogui.write(quantidade)
-    #  3 - clicar e digitar preço (x=442, y=428)
-    pyautogui.click(x=442, y=428)
-    pyautogui.write(preco)
-    #  4 - clicar em registrar (x=306, y=582)
-    pyautogui.click(x=306, y=582,duration=0.5)
-    sleep(1)
+        produto, quantidade, preco = linha.strip().split(',')
+
+        # 1 - clicar e digitar produto (x=470, y=373)
+        pyautogui.click(x=470, y=373, duration=0.5)
+        pyautogui.write(produto)
+
+        # 2 - clicar e digitar quantidade (x=442, y=404)
+        pyautogui.click(x=442, y=404, duration=0.5)
+        pyautogui.write(quantidade)
+
+        # 3 - clicar e digitar preço (x=442, y=428)
+        pyautogui.click(x=442, y=428)
+        pyautogui.write(preco)
+
+        # 4 - clicar em registrar (x=306, y=582)
+        pyautogui.click(x=306, y=582, duration=0.5)
+        sleep(1)
